676508b49befae37394099404fb076cd2ea90850
Navid Shad <navidshad72@gmail.com>
2020-11-26 18:30:36 +0330

    server - added default port.

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const ModularRest = require('modular-rest');
 const Path = require('path');
 
 ModularRest.createRest({
+    port: '3001',
     componentDirectory: Path.join(__dirname, 'src/services'),
     uploadDirectory: Path.join(__dirname, 'uploads')
 })
