46a5dfdc100ea4b19d990e25a68cbf786ad28bcb
Navid Shad <navidshad72@gmail.com>
2020-12-03 23:49:51 +0330

    website - added collection manager components

---
+++ website/components/database/CollectionForm.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="flex flex-col">
+    <div v-for="(field, i) in fieldsToShow" :key="i" class="mt-8">
+      <vs-input
+        block
+        :label="field.title || field.key"
+        :type="field.type"
+        :placeholder="field.placeholder"
+        :value="form[field.key]"
+        @input="form[field.key] = $event"
+      >
+        <template #icon v-if="field.icon">
+          <i :class="[field.icon]" />
+        </template>
+      </vs-input>
+    </div>
+
+    <div class="mt-8">
+      <vs-button class="mt-16" v-if="!edit" @click="create">{{
+        $t('create')
+      }}</vs-button>
+      <vs-button class="mt-16" v-else @click="update">{{ $t('update') }}</vs-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { DataProvider } from '@mres/web'
+const dataProvider = DataProvider.getInstance()
+
+export default {
+  props: {
+    database: { type: String, required: true },
+    collection: { type: String, required: true },
+    /**
+     * An array of
+     * {type, title, key, placeholder, icon}
+     */
+    fields: { type: Array, default: () => [] },
+    /**
+     * Define wheather or not this form
+     * is an edit form.
+     */
+    edit: Boolean,
+    /**
+     * Document id to edit it
+     * when edit is true.
+     */
+    id: String,
+  },
+
+  data() {
+    return {
+      form: {},
+      pending: false,
+    }
+  },
+
+  computed: {
+    fieldsToShow() {
+      return this.fields.filter(
+        (item) => item.hideInform == undefined || item.hideInform == false
+      )
+    },
+  },
+
+  methods: {
+    update() {
+      this.pending = true
+    },
+
+    create() {
+      this.pending = true
+      dataProvider
+        .insertOne({
+          database: this.database,
+          collection: this.collection,
+          document: this.form,
+        })
+        .finally(() => (this.pending = false))
+    },
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ website/components/database/CollectionViewer.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <!-- Header -->
+    <div class="flex justify-between items-center flex-row-reverse">
+      <h2 class="text-lg">{{ $t('artist.title') }}</h2>
+      <vs-button @click="activeForm = true">{{ $t('artist.add') }}</vs-button>
+    </div>
+
+    <!-- Content -->
+    <card class="p-8 mt-4">
+      <vs-table>
+        <!-- Search -->
+        <template slot="header">
+          <vs-input border block placeholder="Search" type="search" />
+        </template>
+
+        <!-- THead -->
+        <template slot="thead">
+          <vs-tr>
+            <vs-th v-for="(field, i) in fields" :key="i">
+              {{ field.key }}
+            </vs-th>
+          </vs-tr>
+        </template>
+
+        <!-- TBody -->
+        <template slot="tbody">
+          <vs-tr v-for="(row, i) in list" :key="i">
+            <vs-td v-for="(field, i) in fields" :key="i">
+              {{ row[field.key] }}
+            </vs-td>
+          </vs-tr>
+        </template>
+      </vs-table>
+    </card>
+
+    <!-- Ceate/Edit form -->
+    <vs-dialog prevent-close :value="activeForm" @input="activeForm = $event">
+      <collection-form
+        :database="database"
+        :collection="collection"
+        :fields="fields"
+      />
+    </vs-dialog>
+  </div>
+</template>
+
+<script>
+import { DataProvider } from '@mres/web'
+const dataProvider = DataProvider.getInstance()
+
+export default {
+  props: {
+    database: { type: String, required: true },
+    collection: { type: String, required: true },
+    /**
+     * An array of
+     * {type, title, key, placeholder, icon}
+     */
+    fields: { type: Array, default: () => [] },
+  },
+
+  data() {
+    return {
+      list: [],
+      pending: false,
+      activeForm: false,
+    }
+  },
+
+  created() {
+    this.find()
+  },
+
+  computed: {
+    findOption() {
+      return {
+        database: this.database,
+        collection: this.collection,
+        query: {},
+      }
+    },
+  },
+
+  methods: {
+    find() {
+      this.pending = true
+      dataProvider
+        .find(this.findOption)
+        .then((list) => (this.list = list))
+        .finally(() => (this.pending = false))
+    },
+  },
+}
+</script>
+
+<style>
+</style>
