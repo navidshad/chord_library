0db8e505d003ed2d89ae08ffac6777962d513f22
Navid Shad <navidshad72@gmail.com>
2020-12-06 11:59:16 +0330

    website - add select input for artist and genre

---
+++ website/components/inputs/SelectArtist.vue
@@ -0,0 +1,77 @@
+<template>
+  <vs-select
+    :loading="pending"
+    :multiple="multiple"
+    :key="list.length"
+    :label="label"
+    :placehoder="$t('artist.select-artist')"
+    v-model="tempValue"
+  >
+    <vs-option
+      v-for="(item, i) in list"
+      :key="i"
+      :label="item.name"
+      :value="item._id"
+    >
+      {{ item.name }}
+    </vs-option>
+  </vs-select>
+</template>
+
+<script>
+import { dataProvider } from '@mres/web'
+import notifier from '../../utilities/notifier'
+
+export default {
+  props: {
+    multiple: { type: Boolean, default: true },
+    value: { default: () => [] },
+    label: String,
+  },
+  data() {
+    return {
+      list: [],
+      pending: false,
+      tempValue: '',
+    }
+  },
+  watch: {
+    value: {
+      immediate: true,
+      deep: true,
+      handler(newValue) {
+        this.value = newValue
+      },
+    },
+    tempValue(value) {
+      this.$emit('input', value)
+    },
+  },
+  created() {
+    this.loadItems()
+  },
+  methods: {
+    loadItems() {
+      this.pending = true
+      dataProvider
+        .find({
+          database: 'chord',
+          collection: 'artist',
+          query: {},
+        })
+        .then((list) => (this.list = list))
+        .catch(({ error }) => {
+          notifier.toast({
+            label: 'Load artists error',
+            description: error,
+            type: 'error',
+          })
+        })
+        .finally(() => (this.pending = false))
+    },
+  },
+}
+</script>
+
+<style>
+</style>
