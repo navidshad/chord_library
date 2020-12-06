a873b313df8bb15b06d96039bcf67561c9956d46
Navid Shad <navidshad72@gmail.com>
2020-12-06 14:54:44 +0330

    website - add new-song page with chord editor component

---
+++ website/components/inputs/ChordEditor.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="editor" class="bg-gray-200" contenteditable="true" @input="content = $event.target.innerText"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+  },
+  data() {
+    return {
+      content: '',
+    }
+  },
+  watch: {
+    value: {
+      immediate: true,
+      handler(value) {
+        if (value) this.content = value
+      },
+    },
+    content() {
+      this.$emit('input', this.content)
+    },
+  },
+}
+</script>
+
+<style scoped>
+#editor {
+    min-height: 400px;
+    padding: 10px;
+    direction: rtl;
+}
+</style>
--- website/pages/admin/new-song.vue
+++ website/pages/admin/new-song.vue
@@ -1,8 +1,78 @@
-<template></template>
+<template>
+  <div>
+    <!-- Header -->
+    <div class="flex justify-between items-center flex-row-reverse">
+      <h2 class="text-lg">{{ $t('song.new-song') }}</h2>
+      <!-- <vs-button to="/admin/new-song">{{ $t('add') }}</vs-button> -->
+    </div>
+    <card class="p-4 mt-4">
+      <vs-input
+        class="my-4"
+        block
+        :label="$t('song.title')"
+        v-model="form.title"
+      />
+      <div class="my-6 flex items-center flex-row-reverse">
+        <select-artist
+          block
+          :label="$t('artist.artists')"
+          v-model="form.artists"
+        />
+        <select-genre
+          class="mr-4"
+          block
+          :label="$t('genre.genres')"
+          v-model="form.genres"
+        />
+      </div>
+      <chord-editor v-model="form.content" />
+
+      <div class="mt-4">
+        <vs-button :loading="pending" @click="create">{{
+          $t('create')
+        }}</vs-button>
+      </div>
+    </card>
+  </div>
+</template>
 
 <script>
+import { dataProvider } from '@mres/web'
+import notifier from '../../utilities/notifier'
+
 export default {
   middleware: ['auth'],
+  data() {
+    return {
+      pending: false,
+      form: {
+        title: '',
+        artists: [],
+        genres: [],
+        content: 'enter something',
+      },
+    }
+  },
+  methods: {
+    create() {
+      this.pending = true
+      dataProvider
+        .insertOne({
+          database: 'chord',
+          collection: 'song',
+          doc: this.form,
+        })
+        .then(() => this.$router.push('/admin/songs'))
+        .catch(({ error }) => {
+          notifier.toast({
+            label: 'Create song error',
+            description: JSON.stringify(error),
+            typr: 'error',
+          })
+        })
+        .finally(() => (this.pending = false))
+    },
+  },
 }
 </script>
 
