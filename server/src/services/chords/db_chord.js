ef9853aaa6b3a9645ffed1097f59e56e642428fd
Navid Shad <navidshad72@gmail.com>
2020-12-13 12:41:29 +0330

    server - improve database shemas

--- server/src/services/chords/db_chord.js
+++ server/src/services/chords/db_chord.js
@@ -1,11 +1,20 @@
 const { CollectionDefinition, Schema, Permission, PermissionTypes, DatabaseTrigger, } = require('@modular-rest/server');
 
+
+let TableRowSchema = new Schema({
+    major: { type: Schema.Types.ObjectId, ref: 'chord', required: true },
+    naturalMinor: { type: Schema.Types.ObjectId, ref: 'chord', required: true },
+    harmonicMinor: { type: Schema.Types.ObjectId, ref: 'chord', required: true },
+    melodicMinor: { type: Schema.Types.ObjectId, ref: 'chord', required: true },
+})
+
 module.exports = [
     new CollectionDefinition({
         db: 'chord',
         collection: 'type',
         schema: new Schema({
-            title: { type: String, unique: true },
+            title: { type: String, unique: true, required: true },
+            description: String,
         }),
         permissions: [
             new Permission({
@@ -24,8 +33,29 @@ module.exports = [
         db: 'chord',
         collection: 'degree',
         schema: new Schema({
-            minor: String,
-            major: String,
+            minor: { type: String, unique: true, required: true },
+            major: { type: String, unique: true, required: true },
+            description: String,
+        }),
+        permissions: [
+            new Permission({
+                type: PermissionTypes.god_access,
+                write: true,
+                read: true,
+            }),
+            new Permission({
+                type: PermissionTypes.anonymous_access,
+                read: true,
+            })
+        ],
+    }),
+
+    new CollectionDefinition({
+        db: 'chord',
+        collection: 'chord',
+        schema: new Schema({
+            title: { type: String, unique: true, required: true },
+            type: { type: Schema.Types.ObjectId, ref: 'type', required: true, },
         }),
         permissions: [
             new Permission({
@@ -45,8 +75,8 @@ module.exports = [
         collection: 'table',
         schema: new Schema({
             degree: { type: Schema.Types.ObjectId, ref: 'degree', required: true, },
-            type: { type: Schema.Types.ObjectId, ref: 'degree', required: true, },
-            table: Array,
+            type: { type: Schema.Types.ObjectId, ref: 'type', required: true, },
+            rows: [TableRowSchema],
         }),
         permissions: [
             new Permission({
