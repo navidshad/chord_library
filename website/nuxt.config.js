b086ecbd42465115391aec10e03271d11add2cd0
somayeh roohani <somayehroohani06@gmail.com>
2022-08-15 12:46:26 +0300

    [ Task ] Add Vuetify 2 into the old project

--- website/nuxt.config.js
+++ website/nuxt.config.js
@@ -92,6 +92,7 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/vuetify',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
