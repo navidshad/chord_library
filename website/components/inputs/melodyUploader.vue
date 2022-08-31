24d41337c9835e64eff0308122c36a27cc68787f
somayeh roohani <somayehroohani06@gmail.com>
2022-08-31 13:24:56 +0300

    [ Task ] Convert All Vuesax components to Vuetify equivalent( inputs -> melodyUploader)

--- website/components/inputs/melodyUploader.vue
+++ website/components/inputs/melodyUploader.vue
@@ -1,34 +1,34 @@
 <template>
-<div>
+  <div>
     <div>
-        <h3>{{$t('melody-uploader.title')}}</h3>
+      <h3>{{ $t("melody-uploader.title") }}</h3>
     </div>
     <div>
-        <div v-for="(melody, i) in tempMelodies" :key="i"></div>
+      <div v-for="(melody, i) in tempMelodies" :key="i"></div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
   props: {
-      melodies: Array,
+    melodies: Array,
   },
   watch: {
-      melodies: {
-          immediate: true,
-          deep: true,
-          handler(newVal) {
-              this.tempMelodies = newVal;
-          }
-      }
+    melodies: {
+      immediate: true,
+      deep: true,
+      handler(newVal) {
+        this.tempMelodies = newVal;
+      },
+    },
   },
   data() {
-      return {
-          tempMelodies: [],
-      }
-  }
-}
+    return {
+      tempMelodies: [],
+    };
+  },
+};
 </script>
 
 <style>
