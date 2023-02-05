403b5daecc560e02c56484e2dc3ff1ae2b07f5bd
Navid Shad <navidshad72@gmail.com>
2023-02-05 17:49:27 +0300

    [ Task ] Convert DialogPresentor to Vuetify component.

--- website/components/notifiers/DialogPresentor.vue
+++ website/components/notifiers/DialogPresentor.vue
@@ -1,26 +1,28 @@
 <template>
-  <vs-dialog
+  <v-dialog
     prevent-close
     :value="isActive"
     @input="destroyModalContent"
     v-bind="modalOptions"
   >
-    <component
-      v-if="currentModalContent"
-      :is="currentModalContent"
-      v-bind="props"
-      v-on="events"
-      @close="destroyModalContent"
-    />
-  </vs-dialog>
+    <v-card class="p-4">
+      <component
+        v-if="currentModalContent"
+        :is="currentModalContent"
+        v-bind="props"
+        v-on="events"
+        @close="destroyModalContent"
+      />
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
-import EventBus from '../../utilities/event-bus'
-import AlertDialog from './AlertDialog'
+import EventBus from "../../utilities/event-bus";
+import AlertDialog from "./AlertDialog";
 
 export default {
-  name: 'dialog-presentor',
+  name: "dialog-presentor",
   data() {
     return {
       isActive: false,
@@ -29,72 +31,72 @@ export default {
       props: {},
       events: {},
       modalOptions: {},
-    }
+    };
   },
   created() {
-    EventBus.listen('show-alert-dialog', this.onAlertDialog)
-    EventBus.listen('show-dialog', this.onDialog)
+    EventBus.listen("show-alert-dialog", this.onAlertDialog);
+    EventBus.listen("show-dialog", this.onDialog);
   },
   destroyed() {
-    EventBus.remove('show-alert-dialog', this.onAlertDialog)
-    EventBus.remove('show-dialog', this.onDialog)
+    EventBus.remove("show-alert-dialog", this.onAlertDialog);
+    EventBus.remove("show-dialog", this.onDialog);
   },
   methods: {
     onAlertDialog({ title, description, actions = [], onAction }) {
-      this.isActive = true
+      this.isActive = true;
 
       // normalize actions
       let normalizedActions = actions.map((item) => {
-        if (typeof item == 'string') {
+        if (typeof item == "string") {
           return {
             label: item,
             button: { shadow: true, transparent: true },
             pending: false,
-          }
+          };
         } else {
-          item['pending'] = false
-          return item
+          item["pending"] = false;
+          return item;
         }
-      })
+      });
 
-      this.currentModalContent = AlertDialog
+      this.currentModalContent = AlertDialog;
 
       this.props = {
         title,
         description,
         actions: normalizedActions,
         pending: this.pending,
-      }
+      };
 
-      let destroyModalContent = this.destroyModalContent
+      let destroyModalContent = this.destroyModalContent;
       this.events = {
         actionClick(action) {
-          onAction(action.label, action, destroyModalContent)
+          onAction(action.label, action, destroyModalContent);
         },
-      }
+      };
     },
 
     onDialog({ component, props = {}, events = {}, modal = {} }) {
-      this.isActive = true
+      this.isActive = true;
 
-      this.currentModalContent = component
+      this.currentModalContent = component;
       this.props = {
         ...props,
         closeModal: this.destroyModalContent,
-      }
-      this.events = events
-      this.modalOptions = modal
+      };
+      this.events = events;
+      this.modalOptions = modal;
     },
 
     destroyModalContent() {
-      this.isActive = false
-      this.currentModalContent = null
-      this.modalOptions = {}
-      this.props = {}
-      this.events = {}
+      this.isActive = false;
+      this.currentModalContent = null;
+      this.modalOptions = {};
+      this.props = {};
+      this.events = {};
     },
   },
-}
+};
 </script>
 
 <style>
